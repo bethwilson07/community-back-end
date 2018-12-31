aa7407ead527c6a61335c06687f6af50a8aba380
bethwilson07 <beth.ny.wlsn@gmail.com>
2018-12-31 13:12:54 -0500

    updated seed data

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -1,5 +1,5 @@
 class EventSerializer < ActiveModel::Serializer
-  attributes :id, :name, :description, :location, :latitude, :longitude, :when, :group_id
+  attributes :id, :name, :description, :location, :latitude, :longitude, :when, :photo, :group_id
   belongs_to :group
   has_many :member_events
   has_many :members, through: :member_events
